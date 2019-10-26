98a3a935d0a0cc497e3b322256211b54aac7767c
ReSummit <star9123.dv@gmail.com>
2019-10-26 00:39:55 -0700

    Changed folder where initial website is at and (maybe) fixed styling

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const http = require('http');
 const app = express();
 const server = http.createServer(app);
+app.use(express.static(__dirname + "/root"));
 
 // Server will always find an open port.
 const port = process.env.PORT || 3001;
@@ -9,12 +10,7 @@ server.listen(port, '0.0.0.0', () => {
     console.log(`Server listening on port ${port}`);
 });
 
-// Access example.com/
-app.get('/', (req, res) => {
-    res.send("I love HackSchool.");
-});
-
 // Access example.com/memes
-app.get('/index.html', (req, res) => {
+app.get('/memes', (req, res) => {
     res.sendFile(__dirname + "/index.html");
 });
