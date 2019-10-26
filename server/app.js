61db24459da5d38663935e498b2a3241e872404c
ReSummit <star9123.dv@gmail.com>
2019-10-26 11:15:17 -0700

    Changed location of files (again)

--- server/app.js
+++ server/app.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const http = require('http');
+const AWS = require('aws-sdk');
+const fs = require('fs');
 const app = express();
 const server = http.createServer(app);
-app.use(express.static(__dirname + "/root"));
+app.use(express.static(__dirname));
+
+// Auth ID's for S3 implementation (will be redacted and invalidated later)
+const ID = 'AKIAIY3ST4OACMJZG3EA';
+const SECRET = 'zOE8N4f+ENBQdRy2vB6AjLIbur9EkWDHFguzUAO3';
+
+// Create the S3 object
+const s3 = new AWS.S3({
+    accessKeyId: ID,
+    secretAccessKey: SECRET
+});
 
 // Server will always find an open port.
 const port = process.env.PORT || 3001;
@@ -10,7 +22,16 @@ server.listen(port, '0.0.0.0', () => {
     console.log(`Server listening on port ${port}`);
 });
 
-// Access example.com/memes
-app.get('/memes', (req, res) => {
-    res.sendFile(__dirname + "/index.html");
-});
+function uploadFnct() {
+    const fileContent = fs.readFileSync($('#input').val());
+
+    const params = {
+        Bucket: freejukebox.space,
+        Key: 'Heh',
+        Body: fileContent
+    }
+}
+
+function sayFileName() {
+    console.log($('#input').val());
+}
