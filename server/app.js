397b62deb0c92168992453d00c50d462553693a7
ReSummit <star9123.dv@gmail.com>
2019-10-26 17:23:32 -0700

    Adjusted index for detecting audio stream changes (also merge)

--- server/app.js
+++ server/app.js
@@ -2,25 +2,11 @@ const express = require('express');
 const http = require('http');
 const AWS = require('aws-sdk');
 const fs = require('fs');
-
-const bodyParser = require('body-parser');
-const multer = require('multer');
-
 const app = express();
 const server = http.createServer(app);
 app.use(express.static(__dirname));
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(multer({ dest: '/tmp/'}));
 
-// Auth ID's for S3 implementation (will be redacted and invalidated later)
-const ID = '(Redacted)';
-const SECRET = '(Redacted)';
 
-// Create the S3 object
-const s3 = new AWS.S3({
-    accessKeyId: ID,
-    secretAccessKey: SECRET
-});
 
 // Server will always find an open port.
 const port = process.env.PORT || 3001;
@@ -28,36 +14,26 @@ server.listen(port, '0.0.0.0', () => {
     console.log(`Server listening on port ${port}`);
 });
 
-app.get('/POST', (req, res) => {
-    res.send("I love HackSchool.");
-    console.log(req.files.file.name);
-    console.log(req.files.file.path);
-    console.log(req.files.file.type);
-    var file = __dirname + "/" + req.files.file.name;
-    
-    fs.readFile( req.files.file.path, function (err, data) {
-			response = {
-				message:'File uploaded successfully',
-				filename:req.files.file.name
-			};
-			console.log( response );
-			res.end( JSON.stringify( response ) );
-    });
+app.post('/songStatus', async (req, res) => {
+    console.log(req);
+
+    // Respond with a successful prompt.
+    response = {
+        message:'File uploaded successfully',
+        filename: "Debug"
+    };
+    res.end( JSON.stringify( response ) );
 });
 
-function uploadFnct(file) {
-    const fileContent = fs.readFileSync(file);
+app.post('/setTime', async (req, res) => {
+    console.log(req);
 
-    const params = {
-        Bucket: "freejukebox.space",
-        Key: 'Heh',
-        Body: fileContent
-    }
+    // Respond with a successful prompt.
+    response = {
+        message:'File uploaded successfully',
+        filename: "Debug"
+    };
+    res.end( JSON.stringify( response ) );
+});
 
-    s3.upload(params, function(err, data) {
-        if (err) {
-            throw err;
-        }
-        console.log(`File uploaded successfully. ${data.Location}`);
-    });
-}
+
