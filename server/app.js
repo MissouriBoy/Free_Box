dbbf18495714f8a4e75c392a6254309c0973a2ab
ReSummit <star9123.dv@gmail.com>
2019-10-26 20:37:53 -0700

    Finally some syncing going on.

--- server/app.js
+++ server/app.js
@@ -5,7 +5,7 @@ const server = http.createServer(app);
 app.use(express.static(__dirname));
 
 var WebSocketServer = require('ws').Server,
-    wss = new WebSocketServer({port: 3000}),
+    wss = new WebSocketServer({port: 8080}),
     CLIENTS=[];
 
 wss.on('connection', function(ws) {
@@ -14,12 +14,11 @@ wss.on('connection', function(ws) {
         console.log('received: %s', message);
         sendAll(message);
     });
-    ws.send("NEW USER JOINED");
 });
 
 function sendAll (message) {
     for (var i=0; i<CLIENTS.length; i++) {
-        CLIENTS[i].send("Message: " + message);
+        CLIENTS[i].send(message);
     }
 }
 
@@ -27,26 +26,4 @@ function sendAll (message) {
 const port = process.env.PORT || 3001;
 server.listen(port, '0.0.0.0', () => {
     console.log(`Server listening on port ${port}`);
-});
-
-app.post('/songStatus', async (req, res) => {
-    console.log(req);
-
-    // Respond with a successful prompt.
-    response = {
-        message:'File uploaded successfully',
-        filename: "Debug"
-    };
-    res.end( JSON.stringify( response ) );
-});
-
-app.post('/setTime', async (req, res) => {
-    console.log(req);
-
-    // Respond with a successful prompt.
-    response = {
-        message:'File uploaded successfully',
-        filename: "Debug"
-    };
-    res.end( JSON.stringify( response ) );
 });
