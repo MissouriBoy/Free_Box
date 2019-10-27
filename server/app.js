ec19025398661cb0ac897071f5b4124986651120
MissouriBoy <missouriboy12345@yahoo.com>
2019-10-26 19:18:49 -0700

    stuff

--- server/app.js
+++ server/app.js
@@ -1,12 +1,27 @@
 const express = require('express');
 const http = require('http');
-const AWS = require('aws-sdk');
-const fs = require('fs');
 const app = express();
 const server = http.createServer(app);
 app.use(express.static(__dirname));
 
+var WebSocketServer = require('ws').Server,
+    wss = new WebSocketServer({port: 3000}),
+    CLIENTS=[];
 
+wss.on('connection', function(ws) {
+    CLIENTS.push(ws);
+    ws.on('message', function(message) {
+        console.log('received: %s', message);
+        sendAll(message);
+    });
+    ws.send("NEW USER JOINED");
+});
+
+function sendAll (message) {
+    for (var i=0; i<CLIENTS.length; i++) {
+        CLIENTS[i].send("Message: " + message);
+    }
+}
 
 // Server will always find an open port.
 const port = process.env.PORT || 3001;
